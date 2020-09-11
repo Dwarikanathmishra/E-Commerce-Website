5208676b3d80cf2a0c2244ae980d01f9f55ec4c1
Dwarikanathmishra <dwarikanathmishra7@gmail.com>
2020-09-11 17:08:06 +0530

    added checkout page

--- src/Product.js
+++ src/Product.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './Product.css'
+import { useStateValue } from './StateProvider';
 
 function Product({id ,title ,image ,price ,rating}) {
+    const[state, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        //dispatch the item into data layer
+        dispatch({
+            type: 'ADD_TO_BASKET' ,
+            item:{
+                id:id,
+                title:title,
+                image:image,
+                price:price,
+                rating:rating,
+            },
+        });
+    };
+
     return (
       <div className="product">
         <div className="product__info">
           <p>{title}</p>
 
           <p className="product__price">
-            <small>₹</small>
+            <small>$</small>
             <strong>{price}</strong>
           </p>
 
@@ -23,7 +40,7 @@ function Product({id ,title ,image ,price ,rating}) {
 
         <img src={image} alt="" />
 
-        <button>Add to Basket</button>
+        <button onClick={addToBasket}>Add to Basket</button>
       </div>
     );
 }
